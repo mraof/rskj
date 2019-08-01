8f893464da975e41ca316d1cdab6f91eb447c1a7
nacho692 <mariotti.ignacio@gmail.com>
2019-08-01 11:02:47 -0300

    Add sync state factory

---
+++ rskj-core/src/main/java/co/rsk/net/statesync/Factory.java
@@ -0,0 +1,12 @@
+package co.rsk.net.statesync;
+
+
+public class Factory {
+
+    private final SyncConfiguration syncConfiguration;
+
+    public Factory(SyncConfiguration syncConfiguration) {
+        this.syncConfiguration = syncConfiguration;
+    }
+
+}
