6cab44bf044b8d9e6a2274642316e5fcc23f3783
nacho692 <mariotti.ignacio@gmail.com>
2019-08-01 11:00:20 -0300

    Add sync state interface

---
+++ rskj-core/src/main/java/co/rsk/net/statesync/StateSyncState.java
@@ -0,0 +1,27 @@
+package co.rsk.net.statesync;
+
+import co.rsk.net.NodeID;
+import co.rsk.trie.Trie;
+import org.ethereum.core.BlockHeader;
+import org.ethereum.core.Transaction;
+
+import java.time.Duration;
+import java.util.List;
+
+public interface StateSyncState {
+
+    StateSyncState newPeerStatus();
+
+    StateSyncState tick(Duration duration);
+
+    StateSyncState newTrieNode(NodeID peerId, long requestId, Trie trieNode);
+
+    StateSyncState newBlockHeaders(List<BlockHeader> chunk);
+
+    StateSyncState newBody(NodeID peerId,
+                           long requestId,
+                           List<Transaction> transactions,
+                           List<BlockHeader> uncles);
+
+    StateSyncState onEnter();
+}
